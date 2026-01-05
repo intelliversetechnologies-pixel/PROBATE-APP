2f11ccba00cc1f9637ac3d7f71341b689659ea7c
FrisSoftwareteam <info@firstregistrarsnigeria.com>
2026-01-05 15:32:44 +0100

    Fix header CSS import case

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Header.css';
+import './header.css';
 
 function Header() {
   const navigate = useNavigate();
