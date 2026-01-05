d512a926807c835424e29fb407e2bceaf35582fb
FrisSoftwareteam <info@firstregistrarsnigeria.com>
2026-01-05 15:48:34 +0100

    Update Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Hero from '../pages/hero'
 import Services from '../pages/services'
 import About from '../pages/about'
 import Review from '../pages/review'
-import Contact from '../pages/contact'
+import Contact from './contact'
 import Footer from '../components/footer'
 
 const Home = () => {
