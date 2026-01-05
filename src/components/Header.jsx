68ed4689cdcf98e873e14a250a2e05c05d843805
FrisSoftwareteam <info@firstregistrarsnigeria.com>
2026-01-05 15:57:32 +0100

    Fix component case and ignore dist

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './header.css';
+import './Header.css';
 
 function Header() {
   const navigate = useNavigate();
